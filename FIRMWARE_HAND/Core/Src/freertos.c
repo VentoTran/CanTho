8b38ce70b6d308b383dd85c95d0d7deaf33fc0f3
Vento <zseefvhu12345@gmail.com>
2022-03-25 12:27:54 +0700

    comment

--- FIRMWARE_HAND/Core/Src/freertos.c
+++ FIRMWARE_HAND/Core/Src/freertos.c
@@ -82,7 +82,7 @@ uint8_t is_done = 0;
 uint8_t is_fall = 0;
 uint8_t is_MPU_OK = 0;
 
-char call[25] = "ATD+84816056426;\r\n";
+// char call[25] = "ATD+84816056426;\r\n";
 char SMSStrg[110] = "BENH NHAN DA BI NGA!\nToa do benh nhan: https://www.google.com/maps/search/?api=1&query=10.035911,105.783660\n";
 // char InfoBV1[110] = "Benh vien Da khoa Hoan My Cuu Long, SDT: 02923917901\nBenh vien Da khoa S.I.S Can Tho, SDT: 18001115\n";
 // char InfoBV2[110] = "Benh vien Da khoa Thanh pho Can Tho, SDT: 02923821235\nBenh vien Da khoa Trung uong Can Tho, SDT: 0901215115\n";
@@ -169,6 +169,7 @@ void MX_FREERTOS_Init(void) {
 
 }
 
+
 /* USER CODE BEGIN Header_MQTT_Task */
 /**
   * @brief  Function implementing the MQTT thread.
@@ -180,6 +181,7 @@ void MQTT_Task(void *argument)
 {
   /* USER CODE BEGIN MQTT_Task */
 
+  //  Khoi tao cac tham so cho ket noi qua giao thuc MQTT
   SIM800.sim.apn = "m3-world";
   SIM800.sim.apn_user = "mms";
   SIM800.sim.apn_pass = "mms";
@@ -190,11 +192,12 @@ void MQTT_Task(void *argument)
   SIM800.mqttClient.pass = "";
   SIM800.mqttClient.keepAliveInterval = 120;
 
+  // Chuan bi nghe phan hoi tu module SIM
   osDelay(100);
   HAL_UART_Receive_IT(UART_SIM800, &rx_data, 1);
 
-  // SIM800_SendCommand("AT\r\n", "\r\n", CMD_DELAY);
 
+  //Kiem tra module SIM hoat dong chua? Neu chua thi bat module SIM
   if (SIM800_SendCommand("AT\r\n", "OK\r\n", CMD_DELAY) == SIM_R_nOK)
   {
     HAL_GPIO_WritePin(SIM_PWRKEY_GPIO_Port, SIM_PWRKEY_Pin, 1);
@@ -203,13 +206,16 @@ void MQTT_Task(void *argument)
   }
   
   osDelay(4000);
-
+  // Reset CLK timeout
   if (is_MPU_OK == 1)     HAL_IWDG_Refresh(&hiwdg);
 
+  // Bat dau ket noi MQTT
   MQTT_Init();
 
+  // Reset CLK timeout
   if (is_MPU_OK == 1)     HAL_IWDG_Refresh(&hiwdg);
 
+  // 4 lenh cai dat chuc nang cho module SIM
   SIM800_SendCommand("AT+CMGF=1\r\n", "OK\r\n", 1000);
 
   SIM800_SendCommand("AT+CFUN=1\r\n", "OK\r\n", 1000);
@@ -221,11 +227,13 @@ void MQTT_Task(void *argument)
   // SIM800_SendCommand("AT+CSDH?\r\n", "OK\r\n", 1000);
   SIM800_SendCommand("AT+CSMP=17,167,0,0\r\n", "OK\r\n", 1000);
   
-
+  // Reset CLK timeout
   if (is_MPU_OK == 1)     HAL_IWDG_Refresh(&hiwdg);
 
+  // Khoi tao timer 1s Reset CLK tu dong
   osTimerStart(SecTimerHandle, 1000);
-
+  
+  // Ban tin ket noi trang thai dau tien
   MQTT_Pub("mandevices/stroke-medical/human/fall", "0");
   osDelay(2000);
   uint32_t time = HAL_GetTick();
@@ -236,6 +244,7 @@ void MQTT_Task(void *argument)
     osDelay(1000);
     if (is_fall == 1)
     {
+      // BENH NHAN DA NGA, GUI TIN NHAN
       if (SIM800_SendCommand("AT+CMGS=\"0855484556\"\r\n", ">", 1000) == SIM_R_OK)
       {
         osDelay(200);
@@ -244,12 +253,14 @@ void MQTT_Task(void *argument)
         huart1.Instance->DR = 0b00011010;
         HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
         osDelay(5000);
+        // GUI BAN TIN MQTT BENH NHAN NGA
         MQTT_Pub("mandevices/stroke-medical/human/fall", "1");
         osDelay(2000);
         MQTT_Pub("mandevices/stroke-medical/human/fall/loc", Loc);
         osDelay(2000);
         HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
         // HAL_UART_Transmit_IT(UART_SIM800, call, strlen(call));
+        // GOI DIEN THOAI
         SIM800_SendCommand("ATD0855484556;\r\n", "OK\r\n", 2000);
         osDelay(30000);
         SIM800_SendCommand("ATH\r\n", "OK\r\n", 1000);
@@ -260,17 +271,20 @@ void MQTT_Task(void *argument)
     }
     if ((HAL_GetTick() - time) >= 30000)
     {
+      // ban tin ping moi 30s
       MQTT_Pub("mandevices/stroke-medical/human/fall", "0");
       time = HAL_GetTick();
     }
     if ((SIM800.mqttServer.connect == 0) || (SIM800_Status.SIM == 0) || (SIM800_Status.GPRS == 0) || (SIM800_Status.MQTT == 0))
     {
+      // ket noi lai MQTT
       MQTT_Init();
       MQTT_Pub("mandevices/stroke-medical/human/fall", "0");
       time = HAL_GetTick();
     }
     if (is_done == 1)
     {
+      // Da test xong, truyen diem test len Server
       is_done = 0;
       char str[2];
       sprintf(str, "%d\0", gScore);
@@ -285,6 +299,7 @@ void MQTT_Task(void *argument)
   /* USER CODE END MQTT_Task */
 }
 
+
 /* USER CODE BEGIN Header_Sensor_Task */
 /**
 * @brief Function implementing the SENSORs thread.
@@ -297,6 +312,7 @@ void Sensor_Task(void *argument)
   /* USER CODE BEGIN Sensor_Task */
 
   osDelay(1000);
+  // Khoi tao cam bien mpu6050
   while(MPU6050_Init(&hi2c1) == MPU_nOK);
   // osTimerStart(SecTimerHandle, 1000);
   is_MPU_OK = 1;
@@ -304,22 +320,27 @@ void Sensor_Task(void *argument)
   /* Infinite loop */
   for(;;)
   {
+    // READ SENSOR
     MPU6050_Read_Accel(&hi2c1, &MPU6050);
 
+    // CALCULATE GRAVITY
     gGravity = sqrt((MPU6050.Ax)*(MPU6050.Ax) + (MPU6050.Ay)*(MPU6050.Ay) + (MPU6050.Az)*(MPU6050.Az));
 
     if (gGravity == 0)
     {
+      // Gravity = 0 la cam bien k chay, smthg went wrong
       is_MPU_OK = 0;
     }
     if ((gGravity < 0.20) && (is_fall == 0) && (gGravity != 0))
     {
+      // CHECK NGA, BENH NHAN DA NGA
       is_fall = 1;
       HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
       // vTaskResume(MQTTHandle);
     }
     if (is_test == 1)
     {
+      // TEST
       HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
       while(MPU6050_Init(&hi2c1) == MPU_nOK);
       is_test = 0;
@@ -374,16 +395,19 @@ void Sensor_Task(void *argument)
       HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
     }
 
+    // RESET CLK
     HAL_IWDG_Refresh(&hiwdg);
     osDelay(20);
   }
   /* USER CODE END Sensor_Task */
 }
 
+
 /* Callback01 function */
 void Callback01(void *argument)
 {
   /* USER CODE BEGIN Callback01 */
+  // TIMER 1s RESET CLK
   if (is_MPU_OK == 1)     HAL_IWDG_Refresh(&hiwdg);
   /* USER CODE END Callback01 */
 }
@@ -397,6 +421,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     for (int i = 0; i < 100000; i++);
     if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10) == 0)
     {
+      // BUTTON PRESSED, RAISE FLAG
       is_test = 1;
     }
   }
